a78da9b43f85e7d74d39487b34f92c3d15217c27
Utkarsh Thakur <thakurutkarsh22@gmail.com>
2026-07-03 21:55:21 +0530

    Session 3

---
+++ Session3/Middleware/PasswordAuthMiddleware.js
@@ -0,0 +1,17 @@
+const SECRET_SERVER_PASSWORD = "asdf1234";
+
+function PasswordAuthMiddleware(req, res, next) {
+    const header = req.headers; // { "Authorization": "asdf1234" }
+    const authorization = header.authorization; // "asdf1234"
+
+    // req is bad  - 
+    // "asdf1234" !== "asdf1234"
+    if(authorization !== SECRET_SERVER_PASSWORD) {
+        return res.status(401).json({ message: "Unauthorized form middleware" });
+    } else {
+        // req is good - 
+        next();
+    }
+}
+
+module.exports = PasswordAuthMiddleware;
